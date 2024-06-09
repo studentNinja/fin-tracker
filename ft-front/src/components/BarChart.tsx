adb7b2a2c6b2848153446e81efacf7222bcd3863
studentNinja <v.galyts@ukma.edu.ua>
2024-06-09 19:41:24 +0300

    CU-86bz4h59m - Demo profile BarChart

--- ft-front/src/components/BarChart.tsx
+++ ft-front/src/components/BarChart.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+let dataSet = {
+  "01": 15000,
+  "02": 12000,
+  "03": 13000,
+  "04": 10000,
+  "05": 15000,
+  "06": 7000,
+};
+
+export const options = {
+  //   responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = Object.keys(dataSet);
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: dataSet,
+      backgroundColor: "#F29B7F",
+    },
+  ],
+};
 const BarChart = () => {
-
-    let dataSet={
-        "01":15000,
-        "02": 12000,
-        "03": 13000,
-        "04": 10000,
-        "05": 15000,
-        "06": 7000,
-    }
-
-
-
-    return (
-        <div className="spendings-history-diagram">
-
-
-        </div>
-    );
+  return <Bar options={options} data={data} />;
 };
 
 export default BarChart;
