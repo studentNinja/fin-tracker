b182f8f7266765ad053799bc8b9876cc5da6daa8
studentNinja <v.galyts@ukma.edu.ua>
2024-06-21 12:44:57 +0300

    fixed refreshToken when expired , fixed loop on login

--- ft-back/src/utils/tokenUtils.js
+++ ft-back/src/utils/tokenUtils.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 const generateAccessToken = (userId) => {
-    return jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: '30m' });
+    return jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: '30s' });
 };
 
 const generateRefreshToken = (userId) => {
-    return jwt.sign({ userId }, config.JWT_REFRESH_SECRET, { expiresIn: '2d' });
+    return jwt.sign({ userId }, config.JWT_REFRESH_SECRET, { expiresIn: '30s' });
 };
 
 module.exports = {
