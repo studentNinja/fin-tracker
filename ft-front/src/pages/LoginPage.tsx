3f20ccdc5efc6cfa8541754450664e1c1bbc9e90
giginfee <107624747+giginfee@users.noreply.github.com>
2025-03-25 22:03:23 +0200

    fix

--- ft-front/src/pages/LoginPage.tsx
+++ ft-front/src/pages/LoginPage.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { AppDispatch, RootState } from "../app/store";
-import { loginUser } from "../features/auth/authThunks";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { AppDispatch, RootState } from '../app/store';
+import { loginUser } from '../features/auth/authThunks';
 // import {toast} from "react-toastify";
-import { clearError } from "../features/auth/authSlice";
+import { clearError } from '../features/auth/authSlice';
 
 interface ILoginFormInput {
   email: string;
@@ -17,16 +17,16 @@ interface ILoginFormInput {
 const schema = yup.object().shape({
   email: yup
     .string()
-    .required("Введіть пошту")
-    .email("Некоректна пошта")
+    .required('Введіть пошту')
+    .email('Некоректна пошта')
     .matches(
       /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/,
-      "Некоректна пошта"
+      'Некоректна пошта'
     ),
   password: yup
     .string()
-    .required("Введіть пароль")
-    .min(8, "Пароль повинен мати мінімум 8 символів"),
+    .required('Введіть пароль')
+    .min(8, 'Пароль повинен мати мінімум 8 символів'),
   // .matches(
   //   /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,24}$/,
   //   "Пароль повинен містити хоча б одну літеру та одну цифру"
@@ -49,8 +49,14 @@ const LoginPage: React.FC = () => {
   });
 
   useEffect(() => {
-    if (error == "403") {
-      navigate("/payment");
+    if (isAuthenticated) {
+      navigate('/dashboard');
+    }
+  }, [isAuthenticated, navigate]);
+  
+  useEffect(() => {
+    if (error == '403') {
+      navigate('/payment');
     }
   }, [error, navigate]);
 
@@ -76,16 +82,16 @@ const LoginPage: React.FC = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="input-container input-container-1-in-row">
             <label>Пошта</label>
-            <input type="email" {...register("email")} required />
+            <input type="email" {...register('email')} required />
             {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className="input-container input-container-1-in-row">
             <label>Пароль</label>
-            <input type="password" {...register("password")} required />
+            <input type="password" {...register('password')} required />
             {errors.password && <p>{errors.password.message}</p>}
           </div>
           <button className="form-button" type="submit" disabled={loading}>
-            {loading ? "Відбувається вхід..." : "Увійти"}
+            {loading ? 'Відбувається вхід...' : 'Увійти'}
           </button>
           {error && <div className="error-message">{error}</div>}
         </form>
