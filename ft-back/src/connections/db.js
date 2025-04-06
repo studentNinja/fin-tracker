e01f39fe720e4821a99f82465f0774da019361c8
studentNinja <v.galyts@ukma.edu.ua>
2025-04-06 15:15:51 +0300

    CORS Fix

--- ft-back/src/connections/db.js
+++ ft-back/src/connections/db.js
@@ -5,16 +5,16 @@ require('dotenv').config();
 const connectDB = async () => {
   const MONGODB_URI = process.env.MONGODB_URI;
 
-  
-    mongoose.connect(MONGODB_URI, {
-      ssl: false,
-    }).then(() => {
-      console.log('MongoDB connected');
-    }).catch((err) => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1);
-    });
+
+  mongoose.connect(MONGODB_URI, {
+    ssl: true,
+  }).then(() => {
+    console.log('MongoDB connected');
+  }).catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
 };
 
 
-module.exports =  connectDB;
+module.exports = connectDB;
